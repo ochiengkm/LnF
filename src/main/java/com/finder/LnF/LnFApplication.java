c1bef7039af9af0e30ea41b57af83a53ffc74e34
ochiengkm <mosesa.ochieng@gmail.com>
2024-07-20 15:11:42 +0300

    lost and found init

---
+++ src/main/java/com/finder/LnF/LnFApplication.java
@@ -0,0 +1,13 @@
+package com.finder.LnF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnFApplication.class, args);
+	}
+
+}
